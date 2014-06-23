f528747258a53435a56a52615fc94e64744c7508
David Genest <david.genest@ubisoft.com>
2014-06-23 13:45:26 -0400

    initial project files for develop break demonstration

---
+++ develop_break/module.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def main():
+    subprocess.check_output('binary_hello_world')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='setuptools_develop_breaker', 
+    pacakges=find_packages(),
+    scripts=['binary_hello_world.exe']
+)
